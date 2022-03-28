7aceb463dc92457455b139e1e0657e4fc709636e
Vegard M <94170927+CodeVegard@users.noreply.github.com>
2022-03-28 16:31:49 +0200

    added a script to make the header hide/show on scroll on desktop

---
+++ js/sticky_navbar.js
@@ -0,0 +1,27 @@
+// I wanted to have a sticky header on scroll, taking inspiration here:
+// https://www.w3schools.com/howto/howto_js_navbar_sticky.asp 
+
+let width = window.screen.width;
+
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.position = "fixed";
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.position = "fixed";
+    document.getElementById("navbar").style.top = "-60px";
+  }
+  prevScrollpos = currentScrollPos;
+  if (window.scrollY === 0){
+    document.getElementById("navbar").style.position = "absolute";
+    document.getElementById("navbar").style.top = "";
+  }
+  if (width <= 910){
+    console.log("log");
+    document.getElementById("navbar").style.position = "absolute";
+    document.getElementById("navbar").style.top = "";
+  }
+}
+
